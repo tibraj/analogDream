7dda774a4387ef51a5473b73da15b1da806e1d6c
tibraj <tibraj94@gmail.com>
2021-01-29 15:48:51 -0500

    drum products info

---
+++ src/instruments/instrument.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        id: 1,
+        name: 'Teenage Engineering PO-32',
+        price: 89,
+        sounds: 'Synthesis',
+        effects: 'Yes',
+        sequencer: 'Yes',
+        yearOfRelease: 2017,
+        image: 'https://cdn.mos.cms.futurecdn.net/aLH8wsMnDGQ2ZgJMvq7d8P-970-80.jpg.webp'
+    },
+    {
+        id: 2,
+        name: 'Korg Volca Beats',
+        price: 182,
+        sounds: 'Synthesis and Samples',
+        effects: 'Yes',
+        sequencer: 'Yes',
+        yearOfRelease: 2013,
+        image: 'https://cdn.mos.cms.futurecdn.net/HkdQ7jjWpwzDE4242a6n2h-970-80.jpg.webp'
+    },
+    {
+        id: 3,
+        name: 'Roland TR-08',
+        price: 349,
+        sounds: 'ACB Modeling',
+        effects: 'Yes',
+        sequencer: 'Yes',
+        yearOfRelease: 2017,
+        image: 'https://cdn.mos.cms.futurecdn.net/cpj2b3mfHoo2KaAhYMT8sN-970-80.jpg.webp'
+    },
+    {
+        id: 4,
+        name: 'Roland TR-8S',
+        price: 698,
+        sounds: 'ACB Modeling and Sampling',
+        effects: 'Yes',
+        sequencer: 'Yes',
+        yearOfRelease: 2018,
+        image: 'https://cdn.mos.cms.futurecdn.net/QNFqdUzbA4neLq5KaRkMrV-970-80.jpg.webp'
+    },
+    {
+        id: 5,
+        name: 'Elektron Analog Rytm MkII',
+        price: 1549,
+        sounds: 'Analogue and Samples',
+        effects: 'Yes',
+        sequencer: 'Yes',
+        yearOfRelease: 2017,
+        image: 'https://cdn.mos.cms.futurecdn.net/W5THAqGXEdbzvKY3faSaiJ-970-80.jpg.webp'
+    },
+    {
+        id: 6,
+        name: 'Dave Smith Instruments Tempest',
+        price: 2295,
+        sounds: 'Analogue Synthesis and Samples',
+        effects: 'Yes',
+        sequencer: 'Yes',
+        yearOfRelease: 2012,
+        image: 'https://cdn.mos.cms.futurecdn.net/bNXnfi2EppqFFBYfGS9aXA-970-80.jpg.webp'
+    },
+];
