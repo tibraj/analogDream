1a8da7093277910c7bce7f3f964a002d8ca2b567
tibraj <tibraj94@gmail.com>
2021-02-02 17:33:46 -0500

    props top-down

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Item from './components/Item.js'
+import instruments from './instruments/instrument.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <Item />
+      <Item instruments={instruments}/>
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 
-export default function Item({name, price, sounds, effects, sequencer, yearOfRelease, image }) {
+
+export default function Item({instruments}) {
+    console.log(instruments);
+    
     return (
-        <div className='item'>
-            <h1 className='name'>{name}</h1>
-            <img src={image} alt=""/>
-            <h2 className='price'>${price}</h2>
-            <p className='sounds'>Sounds: {sounds}</p>
-            <p className='effects'>Effects: {effects}</p>
-            <p className='sequencer'>Sequencer: {sequencer}</p>
-            <p className='year-of-release'>Year of Release: {yearOfRelease}</p>
-            <button>Add to Cart</button>
-        </div>
+    <div className='items'>
+        {instruments.map(instrument => (
+            <div key={instrument.id}>
+                <h1 className='name'>{instrument.name}</h1>
+                <img src={instrument.image} alt=""/>
+                <h2 className='price'>${instrument.price}</h2>
+                <p className='sounds'>Sounds: {instrument.sounds}</p>
+                <p className='effects'>Effects: {instrument.effects}</p>
+                <p className='sequencer'>Sequencer: {instrument.sequencer}</p>
+                <p className='year-of-release'>Year of Release: {instrument.yearOfRelease}</p>
+                <button>Add to Cart</button>
+            </div>
+        ))}
+    </div>   
     );
+        
 }
