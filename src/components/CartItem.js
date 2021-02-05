4f728b2128ae62e675433e8c1a4261169a0f1fc6
tibraj <tibraj94@gmail.com>
2021-02-05 13:15:42 -0500

    cart item component

---
+++ src/components/CartItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function CartItem({name, cost, quantity}) {
+    return (
+        <div>
+            <div>
+                <h1>{name}</h1>
+                <h2>Quanitity: {quanitity}</h2>
+                <h2>${cost}</h2>
+            </div>
+        </div>
+    );
+}
