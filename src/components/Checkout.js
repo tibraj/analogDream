6a65bb4a9704a089a546e8ba4d7f389954f245f1
tibraj <tibraj94@gmail.com>
2021-02-10 15:36:41 -0500

    checkout form

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -27,6 +27,23 @@ function Checkout(stripe, totalCost) {
     if (status === 'complete') {
         return <div className='checkout'>Your Payment was Successful!</div>
     }
+
+    return (
+        <form className="CheckoutForm" onSubmit={submit}>
+            <h4>Would you like to complete the purchase?</h4>
+            <CardElement />
+            <button
+                className="CheckoutForm-button"
+                type="submit"
+                disabled={status === 'submitting'}
+            >
+            {status === 'submitting' ? 'Submitting' : 'Submit Order'}
+            </button>
+            {status === 'error' && (
+                <div className="CheckoutForm-error">Something went wrong.</div>
+            )}
+        </form>
+    )
 }
 
 export default injectStripe(Checkout);
