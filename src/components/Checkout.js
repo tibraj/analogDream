ab7ba7385cedfb2fdba7589081768f0acc195b61
tibraj <tibraj94@gmail.com>
2021-02-10 15:27:06 -0500

    stripe async await

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,8 +1,29 @@
 import React, {useState} from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
 
-function Checkout() {
-
+function Checkout(stripe, totalCost) {
+    const [status, setStatus] = useState('default');
+    const submit = async (e) => {
+        e.preventDefault();
+        setStatus('Submitting...');
+        try {
+            let {token} = await stripe.createToken({name: 'Name'});
+            let response = await fetch('/.netlify/functions/charge', {
+                method: 'POST',
+                body: JSON.stringify({
+                    amount: totalCost * 100,
+                    token: token.id
+                })
+            });
+            if (response.ok) {
+                setStatus('Complete');
+            } else {
+                throw new Error('Failed Network Response');
+            }
+        } catch(err) {
+            setStatus('error');
+        }
+    };
 }
 
 export default injectStripe(Checkout);
